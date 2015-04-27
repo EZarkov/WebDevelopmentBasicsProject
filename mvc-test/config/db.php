908b044b5f79b8043f859c0b166ddb169e2d53e9
Evstati Zarkov <evstati.zarkov@gmail.com>
2015-04-27 13:59:32 +0300

    Front controller

---
+++ mvc-test/config/db.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: evstati
+ * Date: 27.04.15
+ * Time: 13:48
+ */
---
+++ mvc/FrontController.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: evstati
+ * Date: 27.04.15
+ * Time: 13:40
+ */
+
+namespace GF;
+
+
+class FrontController {
+	private static $_instance = null;
+
+	private function __construct(){
+
+	}
+
+	public function dispatch(){
+		$a = new Routers\DefautRouter();
+		$a->parse();
+	}
+
+	/**
+	 * @return \GF\FrontController
+	 */
+	public static function getInstance(){
+		if (self::$_instance == null){
+			self::$_instance = new \GF\FrontController();
+		}
+		return self::$_instance;
+	}
+}
