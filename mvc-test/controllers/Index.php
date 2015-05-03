79e229b039a4e5b80aeb2413d1c8c087fad9eb42
Evstati Zarkov <evstati.zarkov@gmail.com>
2015-05-03 18:29:56 +0300

    Validation

--- mvc-test/controllers/Index.php
+++ mvc-test/controllers/Index.php
@@ -14,10 +14,17 @@ class Index {
 
 	public function main(){
 
+		$val = new \MVC\Validation();
+
+		$val->setRule('url', 'http://gong.bg/')->setRule('minLength', 'http://gong.bg/', 5);
+		var_dump($val->validate());
+
+
+		/*
 		$view = \MVC\View::getInstance();
 		$view->title = 'Dev Forum';
 		$view->appendToLayout('body', 'body');
 		$view->display('layouts/default');
-
+		*/
 	}
 }
